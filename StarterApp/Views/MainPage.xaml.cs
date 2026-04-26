80a53ae72c02725105727872a321d6a621102be0
Jack Grant <jackgrant77@icloud.com>
2026-04-26 22:35:57 +0000

    Making fixes to previous code

--- StarterApp/Views/MainPage.xaml.cs
+++ StarterApp/Views/MainPage.xaml.cs
@@ -1,12 +1,22 @@
 using StarterApp.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using StarterApp.Views;
 
 namespace StarterApp.Views;
 
 public partial class MainPage : ContentPage
 {
-    public MainPage(MainViewModel viewModel)
+    private readonly ItemsListPage _itemsListPage;
+
+    public MainPage(MainViewModel viewModel, ItemsListPage itemsListPage)
     {
         InitializeComponent();
         BindingContext = viewModel;
+        _itemsListPage = itemsListPage;
+    }
+
+    private async void OnViewItemsClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(_itemsListPage);
     }
 }
