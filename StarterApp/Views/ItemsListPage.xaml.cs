71496c5b49795c87afff7f5a34e38a418d518674
Jack Grant <jackgrant77@icloud.com>
2026-04-27 14:05:25 +0000

    Connected rental request page to item list

--- StarterApp/Views/ItemsListPage.xaml.cs
+++ StarterApp/Views/ItemsListPage.xaml.cs
@@ -1,3 +1,4 @@
+using StarterApp.Database.Models;
 using StarterApp.ViewModels;
 
 namespace StarterApp.Views;
@@ -6,13 +7,19 @@ public partial class ItemsListPage : ContentPage
 {
     private readonly ItemsListViewModel _viewModel;
     private readonly CreateItemPage _createItemPage;
+    private readonly RequestRentalPage _requestRentalPage;
 
-    public ItemsListPage(ItemsListViewModel viewModel, CreateItemPage createItemPage)
+    public ItemsListPage(
+        ItemsListViewModel viewModel,
+        CreateItemPage createItemPage,
+        RequestRentalPage requestRentalPage)
     {
         InitializeComponent();
         BindingContext = viewModel;
+
         _viewModel = viewModel;
         _createItemPage = createItemPage;
+        _requestRentalPage = requestRentalPage;
     }
 
     protected override void OnAppearing()
@@ -24,5 +31,21 @@ public partial class ItemsListPage : ContentPage
     private async void OnCreateItemClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(_createItemPage);
+    }
+
+    private async void OnRequestRentalClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.CommandParameter is Item item)
+        {
+            var viewModel = _requestRentalPage.BindingContext as RequestRentalViewModel;
+
+            if (viewModel is not null)
+            {
+                viewModel.ItemId = item.Id;
+                viewModel.DailyRate = item.DailyRate;
+            }
+
+            await Navigation.PushAsync(_requestRentalPage);
+        }
     }
 }
