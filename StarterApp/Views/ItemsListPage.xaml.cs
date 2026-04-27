c9e39248179975cc136cbaf0a12544ce509872a3
Jack Grant <jackgrant77@icloud.com>
2026-04-27 11:58:59 +0000

    Added auto-refresh to Items list on page load

--- StarterApp/Views/ItemsListPage.xaml.cs
+++ StarterApp/Views/ItemsListPage.xaml.cs
@@ -4,13 +4,21 @@ namespace StarterApp.Views;
 
 public partial class ItemsListPage : ContentPage
 {
+    private readonly ItemsListViewModel _viewModel;
     private readonly CreateItemPage _createItemPage;
 
     public ItemsListPage(ItemsListViewModel viewModel, CreateItemPage createItemPage)
     {
         InitializeComponent();
         BindingContext = viewModel;
+        _viewModel = viewModel;
         _createItemPage = createItemPage;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _viewModel.LoadItemsCommand.Execute(null);
     }
 
     private async void OnCreateItemClicked(object sender, EventArgs e)
