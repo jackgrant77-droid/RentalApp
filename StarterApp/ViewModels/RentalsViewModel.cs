6b292f9ee3b765207fa7bd08f9f33f1bb84093c0
Jack Grant <jackgrant77@icloud.com>
2026-04-28 22:31:30 +0000

    Completed rental viewing feature via API

--- StarterApp/ViewModels/RentalsViewModel.cs
+++ StarterApp/ViewModels/RentalsViewModel.cs
@@ -1,33 +1,39 @@
 using System.Collections.ObjectModel;
 using System.Windows.Input;
-using StarterApp.Database.Data.Repositories;
 using StarterApp.Database.Models;
-
+using StarterApp.Services;
 namespace StarterApp.ViewModels;
-
 public class RentalsViewModel
 {
-    private readonly IRentalRepository _rentalRepository;
-
-    public ObservableCollection<Rental> Rentals { get; } = new();
-
-    public ICommand LoadRentalsCommand { get; }
-
-    public RentalsViewModel(IRentalRepository rentalRepository)
-    {
-        _rentalRepository = rentalRepository;
-        LoadRentalsCommand = new Command(async () => await LoadRentalsAsync());
-    }
-
-    private async Task LoadRentalsAsync()
-    {
-        Rentals.Clear();
-
-        var rentals = await _rentalRepository.GetAllAsync();
-
-        foreach (var rental in rentals)
-        {
-            Rentals.Add(rental);
-        }
-    }
+   private readonly IApiService _apiService;
+   public ObservableCollection<Rental> Rentals { get; } = new();
+   public ICommand LoadRentalsCommand { get; }
+   public RentalsViewModel(IApiService apiService)
+   {
+       _apiService = apiService;
+       LoadRentalsCommand = new Command(async () => await LoadRentalsAsync());
+   }
+   private async Task LoadRentalsAsync()
+   {
+       try
+       {
+           Rentals.Clear();
+           var rentals = await _apiService.GetOutgoingRentalsAsync();
+           await Application.Current.MainPage.DisplayAlert(
+               "Debug",
+               $"Loaded {rentals.Count} rentals",
+               "OK");
+           foreach (var rental in rentals)
+           {
+               Rentals.Add(rental);
+           }
+       }
+       catch (Exception ex)
+       {
+           await Application.Current.MainPage.DisplayAlert(
+               "Error",
+               ex.ToString(),
+               "OK");
+       }
+   }
 }
