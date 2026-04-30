9de1c86f10992e0d541407491b7a403e35d36caf
Jack Grant <jackgrant77@icloud.com>
2026-04-30 12:00:57 +0000

    Set up testiing and  initial tests

--- StarterApp.Tests/ViewModels/ItemListViewModelTests.cs
+++ StarterApp.Tests/ViewModels/ItemListViewModelTests.cs
@@ -1,50 +1,28 @@
 using Xunit;
 
-using Moq;
-
-using StarterApp.Services;
-
-using StarterApp.ViewModels;
-
-using StarterApp.Database.Models;
-
 namespace StarterApp.Tests.ViewModels;
 
-public class ItemsListViewModelTests
+public class ItemListViewModelTests
 
 {
 
     [Fact]
 
-    public async Task LoadItemsAsync_WhenApiReturnsItems_PopulatesItemsCollection()
+    public void TestProject_ShouldRunSuccessfully()
 
     {
 
         // Arrange
 
-        var mockApi = new Mock<IApiService>();
-
-        mockApi.Setup(x => x.GetItemsAsync())
-
-            .ReturnsAsync(new List<Item>
-
-            {
-
-                new Item { Id = 1, Title = "Item 1" },
-
-                new Item { Id = 2, Title = "Item 2" }
-
-            });
-
-        var viewModel = new ItemsListViewModel(mockApi.Object);
+        var expected = true;
 
         // Act
 
-        await viewModel.LoadItemsAsync();
+        var actual = true;
 
         // Assert
 
-        Assert.Equal(2, viewModel.Items.Count);
+        Assert.Equal(expected, actual);
 
     }
 
